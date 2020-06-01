8a726ecf8a8464bb2e80fa0b44cebea45005e5c7
ldansorean <9722740+ldansorean@users.noreply.github.com>
2020-06-01 14:31:25 +0100

    Show the weather status.

--- app/src/main/java/ldansorean/s5whatstheweather/GetWeatherTask.java
+++ app/src/main/java/ldansorean/s5whatstheweather/GetWeatherTask.java
@@ -1,29 +1,38 @@
 package ldansorean.s5whatstheweather;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.util.JsonReader;
 import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
-
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLEncoder;
 
 /**
  * Asynchronous task making a call to openweathermap API to get the weather for a given city.
  */
 public class GetWeatherTask extends AsyncTask<String, Void, String> {
 
-    private final static String WEATHER_API_GET_BY_CITY_URL = "https://samples.openweathermap.org/data/2.5/weather?q={city}&appid=439d4b804bc8187953eb36d2a8c26a02";
+    private final static String WEATHER_API_GET_BY_CITY_URL = "https://openweathermap.org/data/2.5/weather?q={city}&appid=439d4b804bc8187953eb36d2a8c26a02";
     private final static String CITY_KEY = "{city}";
+    private TextView weatherView;
+
+    public GetWeatherTask(TextView weatherView) {
+        this.weatherView = weatherView;
+    }
 
     @Override
     protected String doInBackground(String... cities) {
         try {
-            String urlStr = WEATHER_API_GET_BY_CITY_URL.replace(CITY_KEY, cities[0]);
+            String encodedCityName = URLEncoder.encode(cities[0], "UTF-8");
+            String urlStr = WEATHER_API_GET_BY_CITY_URL.replace(CITY_KEY, encodedCityName);
             InputStream is = new URL(urlStr).openConnection().getInputStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(is));
             StringBuilder result = new StringBuilder();
@@ -45,17 +54,27 @@ public class GetWeatherTask extends AsyncTask<String, Void, String> {
     protected void onPostExecute(String result) {
         super.onPostExecute(result);
 
-        try {
-            JSONObject jsonRoot = new JSONObject(result);
-            JSONArray weather = jsonRoot.getJSONArray("weather");
-            for (int i = 0; i < weather.length(); i++) {
-                JSONObject weatherPart = weather.getJSONObject(i);
-                Log.i("app", "Main is " + weatherPart.getString("main"));
-                Log.i("app", "Description is " + weatherPart.getString("description"));
+        String weatherDescription = "";
+        if (result == null || result.length() == 0) {
+            showErrorMessage();
+        } else {
+            try {
+                JSONObject jsonRoot = new JSONObject(result);
+                JSONArray weather = jsonRoot.getJSONArray("weather");
+                for (int i = 0; i < weather.length(); i++) {
+                    JSONObject weatherPart = weather.getJSONObject(i);
+                    weatherDescription += weatherPart.getString("main") + ": " + weatherPart.getString("description") + "\r\n";
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                Log.i("app", "Error parsing JSON response (" + result + ")");
+                showErrorMessage();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-            Log.i("app", "Error parsing JSON response (" + result + ")");
         }
+        weatherView.setText(weatherDescription);
+    }
+
+    private void showErrorMessage() {
+        Toast.makeText(weatherView.getContext(), "Could not find weather for that city.", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/ldansorean/s5whatstheweather/MainActivity.java
+++ app/src/main/java/ldansorean/s5whatstheweather/MainActivity.java
@@ -2,22 +2,41 @@ package ldansorean.s5whatstheweather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.input.InputManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText cityName;
+    private TextView weather;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        cityName = findViewById(R.id.cityName);
+        weather = findViewById(R.id.weather);
+    }
+
+    public void getTheWeather(View view) {
         try {
-            new GetWeatherTask().execute("London");
+            new GetWeatherTask(weather).execute(cityName.getText().toString());
+
+            InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(weather.getWindowToken(), 0);
         } catch (Exception e) {
             e.printStackTrace();
+            weather.setText("");
         }
     }
 }
